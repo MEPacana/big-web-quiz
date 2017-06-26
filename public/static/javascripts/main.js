f18b98c5ef04839154753d44a10246df16e96e15
Arnelle Balane <arnellebalane@gmail.com>
2017-06-26 19:46:51 +0800

    routing module use constructors

--- public/static/javascripts/main.js
+++ public/static/javascripts/main.js
@@ -1,38 +1,52 @@
 const router = new Router({
     container: '#root',
     routes: {
-        signin: 'template#signin-screen',
-        question: 'template#question-screen'
+        signin: SignInScreen,
+        question: QuestionScreen
     },
     initial: 'signin'
 });
 
 
-const auth = firebase.auth();
-const database = firebase.database();
+function SignInScreen() {
+    const auth = firebase.auth();
+    const handleAuthStateChanged = auth.onAuthStateChanged((user) => {
+        if (user) {
+            router.navigate('question');
+        }
+    });
+    const handleClick = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider);
+    };
 
-auth.onAuthStateChanged((user) => {
-    if (user) {
-        router.navigate('question');
-        database.ref('active-question').on('value', (snapshot) => (
-            displayQuestion(snapshot.val())
-        ));
-    }
-});
+    this.template = 'template#signin-screen';
+
+    this.ready = function() {
+        this.loginBtn = document.querySelector('.login-btn');
+        this.loginBtn.addEventListener('click', handleClick);
+    };
+
+    this.destroy = function() {
+        this.loginBtn.removeEventListener('click', handleClick);
+        handleAuthStateChanged();
+    };
+}
 
 
-const loginBtn = document.querySelector('.login-btn');
-const questionScreen = document.querySelector('.question-screen');
+function QuestionScreen() {
+    const database = firebase.database();
+    const handleRefOnValue = (snapshot) => {
+        console.log(snapshot.val());
+    };
 
-loginBtn.addEventListener('click', (e) => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-});
+    this.template = 'template#question-screen';
 
+    this.ready = function() {
+        database.ref('active-question').on('value', handleRefOnValue);
+    };
 
-function displayQuestion(question) {
-    if (!question) {
-        return undefined;
-    }
-    console.log(question);
+    this.destroy = function() {
+        database.ref('active-question').off('value', handleRefOnValue);
+    };
 }
--- public/static/javascripts/router.js
+++ public/static/javascripts/router.js
@@ -5,20 +5,24 @@ function resolveElement(selector) {
     return document.querySelector(selector);
 }
 
-function Router(options = {}) {
-    this.container = resolveElement(options.container);
-    this.routes = {};
-
-    for (let route in options.routes || {}) {
-        this.routes[route] = resolveElement(options.routes[route]);
-    }
-
-    this.navigate(options.initial);
+function Router({ container, routes = {}, initial }) {
+    this.container = resolveElement(container);
+    this.routes = routes;
+    this.current = null;
+    this.navigate(initial);
 }
 
 Router.prototype.navigate = function(route) {
+    if (this.current) {
+        this.current.destroy();
+    }
+
     if (!(route in this.routes)) {
         throw new Error(`Route "${route} is not defined."`);
     }
-    this.container.innerHTML = this.routes[route].innerHTML;
+    const screen = new this.routes[route];
+    const template = resolveElement(screen.template).innerHTML;
+    this.container.innerHTML = template;
+    screen.ready();
+    this.current = screen;
 };
