c32def30cfba473d7f2516ee15fcce6878282284
Arnelle Balane <arnellebalane@gmail.com>
2017-06-26 17:52:52 +0800

    switch to question screen after sign in

--- public/static/javascripts/main.js
+++ public/static/javascripts/main.js
@@ -3,14 +3,15 @@ const database = firebase.database();
 
 auth.onAuthStateChanged((user) => {
     if (user) {
-        database.ref('active-question').on('value', (snapshot) => {
-            console.log(snapshot.val());
-        });
+        database.ref('active-question').on('value', (snapshot) => (
+            displayQuestion(snapshot.val())
+        ));
     }
 });
 
 
 const loginBtn = document.querySelector('.login-btn');
+const questionScreen = document.querySelector('.question-screen');
 
 loginBtn.addEventListener('click', (e) => {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -18,9 +19,17 @@ loginBtn.addEventListener('click', (e) => {
 });
 
 
-function displayActiveQuestion(question) {
+function displayQuestion(question) {
     if (!question) {
         return undefined;
     }
+    displayScreen('question');
     console.log(question);
+}
+
+function displayScreen(screen) {
+    const screens = Array.from(document.querySelectorAll('.screen'));
+    const activeScreen = document.querySelector(`.screen.${screen}-screen`);
+    screens.forEach((screen) => screen.classList.add('hidden'));
+    activeScreen.classList.remove('hidden');
 }
