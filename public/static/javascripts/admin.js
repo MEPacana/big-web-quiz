7985df776bd8c29731c1edaeaa224fd6ad6b3e24
Arnelle Balane <arnellebalane@gmail.com>
2017-06-28 21:14:30 +0800

    save question to firebase

--- public/static/javascripts/admin.js
+++ public/static/javascripts/admin.js
@@ -17,15 +17,15 @@ function SignInScreen() {
     });
     const handleSubmit = (e) => {
         e.preventDefault();
-        const email = e.target.querySelector('#email').value;
-        const password = e.target.querySelector('#password').value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
         auth.signInWithEmailAndPassword(email, password);
     };
 
     this.template = 'template#signin-screen';
 
     this.ready = function() {
-        this.signInForm = document.querySelector('form');
+        this.signInForm = $('form');
         this.signInForm.addEventListener('submit', handleSubmit);
     };
 
@@ -37,9 +37,61 @@ function SignInScreen() {
 
 
 function AdminScreen() {
+    const database = firebase.database();
+
+    function resetForm() {
+        $('form').question.value = '';
+        $('#choices').innerHTML = '';
+        for (let i = 0; i < 2; i++) {
+            insertChoice();
+        }
+    }
+    function insertChoice() {
+        const choiceTmpl = $('template#choice').innerHTML;
+        const choice = element(choiceTmpl);
+        $('#choices').appendChild(choice);
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        const text = e.target.question.value;
+        const choices = $$('#choices li').map((choice) => {
+            const text = $('input[type="text"]', choice).value;
+            const correct = $('input[type="radio"]', choice).checked;
+            return { text, correct };
+        });
+        database.ref('questions').push({ text, choices }).then(resetForm);
+    }
+    function handleRefOnChildAdded(snapshot) {
+        console.log(snapshot.val());
+    }
+
     this.template = 'template#admin-screen';
 
-    this.ready = function() {};
+    this.ready = function() {
+        resetForm();
+        $('button[type="button"]').addEventListener('click', insertChoice);
+        $('form').addEventListener('submit', handleSubmit);
+        database.ref('questions').on('child_added', handleRefOnChildAdded);
+    };
 
-    this.destroy = function() {};
+    this.destroy = function() {
+        $('button[type="button"]').removeEventListener('click', insertChoice);
+        $('form').removeEventListener('submit', handleSubmit);
+        database.ref('questions').off('child_added', handleRefOnChildAdded);
+    };
+}
+
+
+function $(selector, context=document) {
+    return context.querySelector(selector);
+}
+
+function $$(selector, context=document) {
+    return Array.from(context.querySelectorAll(selector));
+}
+
+function element(template, stage) {
+    stage = stage || document.createElement('div');
+    stage.innerHTML = template;
+    return stage.firstElementChild;
 }
