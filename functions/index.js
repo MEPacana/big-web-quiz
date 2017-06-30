7d121f39bbda6d2368a60e2c44ebc4e0d2be0395
Arnelle Balane <arnellebalane@gmail.com>
2017-07-01 04:29:10 +0800

    make firebase function return promise instance

--- functions/index.js
+++ functions/index.js
@@ -2,19 +2,22 @@ const functions = require('firebase-functions');
 
 exports.updateUserScore = functions.database.ref('users/{userId}/answers/{questionId}').onWrite((e) => {
     const questionId = e.params.questionId;
-    return e.data.adminRef.root.child(`questions/${questionId}/choices`).once('value', (snapshot) => {
-        const choices = snapshot.val();
-        const correctAnswer = choices.findIndex((choice) => choice.correct);
-        const userAnswer = parseInt(e.data.val(), 10);
-        return e.data.ref.parent.parent.child('score').once('value', (snapshot) => {
-            const score = snapshot.val() || 0;
-            if (userAnswer === correctAnswer) {
-                return snapshot.ref.set(score + 1);
-            } else if (e.data.previous.exists()) {
-                return snapshot.ref.set(score - 1);
-            } else {
-                return snapshot.ref.set(score);
-            }
+    return new Promise((resolve, reject) => {
+        e.data.adminRef.root.child(`questions/${questionId}/choices`).once('value', (snapshot) => {
+            const choices = snapshot.val();
+            const correctAnswer = choices.findIndex((choice) => choice.correct);
+            const userAnswer = parseInt(e.data.val(), 10);
+            e.data.ref.parent.parent.child('score').once('value', (snapshot) => {
+                const score = snapshot.val() || 0;
+                if (userAnswer === correctAnswer) {
+                    snapshot.ref.set(score + 1);
+                } else if (e.data.previous.exists()) {
+                    snapshot.ref.set(score - 1);
+                } else {
+                    snapshot.ref.set(score);
+                }
+                resolve(score);
+            });
         });
     });
 });
