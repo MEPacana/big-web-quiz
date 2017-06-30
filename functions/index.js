9138597690a18dcd9ebdd4d0df913cdfab453bbf
Arnelle Balane <arnellebalane@gmail.com>
2017-07-01 04:22:51 +0800

    firebase function to update user score after answering

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.updateUserScore = functions.database.ref('users/{userId}/answers/{questionId}').onWrite((e) => {
+    const questionId = e.params.questionId;
+    return e.data.adminRef.root.child(`questions/${questionId}/choices`).once('value', (snapshot) => {
+        const choices = snapshot.val();
+        const correctAnswer = choices.findIndex((choice) => choice.correct);
+        const userAnswer = parseInt(e.data.val(), 10);
+        return e.data.ref.parent.parent.child('score').once('value', (snapshot) => {
+            const score = snapshot.val() || 0;
+            if (userAnswer === correctAnswer) {
+                return snapshot.ref.set(score + 1);
+            } else if (e.data.previous.exists()) {
+                return snapshot.ref.set(score - 1);
+            } else {
+                return snapshot.ref.set(score);
+            }
+        });
+    });
+});
