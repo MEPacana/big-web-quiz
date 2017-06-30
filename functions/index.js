51b7bca5af0f1143ed6879a602a5aec432d7c8cc
Arnelle Balane <arnellebalane@gmail.com>
2017-07-01 06:54:48 +0800

    firebase function to send notifications to subscribed users

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const webpush = require('web-push');
+const request = require('request');
 
 exports.updateUserScore = functions.database.ref('users/{userId}/answers/{questionId}').onWrite((e) => {
     const questionId = e.params.questionId;
@@ -22,6 +24,51 @@ exports.updateUserScore = functions.database.ref('users/{userId}/answers/{questi
                 }
                 resolve(score);
             });
+        });
+    });
+});
+
+exports.notifyOnActiveQuestion = functions.database.ref('active-question').onWrite((e) => {
+    if (!e.data.exists()) {
+        return undefined;
+    }
+    return new Promise((resolve, reject) => {
+        const data = e.data.val();
+        const notification = {
+            title: 'New Question Available',
+            body: data.text
+        };
+
+        e.data.adminRef.root.child('users').once('value', (snapshot) => {
+            const subscriptions = [];
+            snapshot.forEach((child) => {
+                const user = child.val();
+                if (user.subscription) {
+                    subscriptions.push(user.subscription);
+                }
+            });
+            subscriptions.forEach((subscription) => {
+                if (subscription.keys) {
+                    webpush.sendNotification(subscription, JSON.stringify(notification), {
+                        gcmAPIKey: 'AIzaSyAbpFzMztXs5ri6JukeHPcYobauLzQBUe8'
+                    });
+                } else {
+                    const subscriptionId = subscription.endpoint.split(/\//g).pop();
+                    const options = {
+                        url: 'https://android.googleapis.com/gcm/send',
+                        headers: {
+                            Authorization: 'key=AIzaSyAbpFzMztXs5ri6JukeHPcYobauLzQBUe8'
+                        },
+                        body: {
+                            registration_ids: subscriptionId,
+                            notification: notification
+                        },
+                        json: true
+                    };
+                    request.post(options);
+                }
+            });
+            resolve();
         });
     });
 });
