82dd9f4f82723a78a6b643ec74d1989399aa671b
Arnelle Balane <arnellebalane@gmail.com>
2017-07-01 13:23:01 +0800

    clear leaderboard list every update

--- public/static/javascripts/leaderboard.js
+++ public/static/javascripts/leaderboard.js
@@ -2,6 +2,7 @@ const database = firebase.database();
 const list = document.querySelector('ol');
 
 database.ref('leaderboard').orderByPriority().on('value', (snapshot) => {
+    list.innerHTML = '';
     snapshot.forEach((item) => {
         const data = item.val();
         const li = document.createElement('li');
