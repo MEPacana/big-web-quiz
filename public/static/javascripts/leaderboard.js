8ad4717bb712e8c1306a34b980fa56b92f204957
Arnelle Balane <arnellebalane@gmail.com>
2017-07-01 13:17:47 +0800

    add leaderboard page

---
+++ public/static/javascripts/leaderboard.js
@@ -0,0 +1,11 @@
+const database = firebase.database();
+const list = document.querySelector('ol');
+
+database.ref('leaderboard').orderByPriority().on('value', (snapshot) => {
+    snapshot.forEach((item) => {
+        const data = item.val();
+        const li = document.createElement('li');
+        li.textContent = `${data.name} (${data.score})`;
+        list.appendChild(li);
+    });
+});
